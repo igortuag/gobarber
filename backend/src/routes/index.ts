b5db71607b0aac61ec022f35c04a7d321c6c321f
Igor Tuag <igortuag@gmail.com>
2020-06-11 14:04:12 -0300

    Cadastro de agendamentos

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
