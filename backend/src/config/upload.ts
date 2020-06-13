a06b24e30d8b70747d49d78865f4e6766853580d
Igor Tuag <igortuag@gmail.com>
2020-06-13 14:50:09 -0300

    Servindo arquivos estaticos

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -10,7 +10,7 @@ export default {
     destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash} - ${file.originalname}`;
+      const fileName = `${fileHash}-${file.originalname}`;
 
       return callback(null, fileName);
     },
