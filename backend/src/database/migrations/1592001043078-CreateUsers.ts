502f71e28bbd5c7d7785a223bb06c1ac9355be04
Igor Tuag <igortuag@gmail.com>
2020-06-13 11:05:18 -0300

    Relacionamento nos models

--- backend/src/database/migrations/1592001043078-CreateUsers.ts
+++ backend/src/database/migrations/1592001043078-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1592001043078 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
