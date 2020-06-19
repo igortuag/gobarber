bc18529f225541547baa912a5724beee93f97f5b
Igor Tuag <igortuag@gmail.com>
2020-06-19 19:15:13 -0300

    Pagina de Cadastro

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn/index';
+import SignUp from './pages/SignUp/index';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
