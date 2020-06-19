5470ba6d2d14bb918967aa99071145cf6c402637
Igor Tuag <igortuag@gmail.com>
2020-06-19 08:11:53 -0300

    Pagina de login

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn/index';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World!</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
